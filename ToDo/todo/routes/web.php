f7a5b888cb279a7dbb15c36a0a6b3966415d5754
Josh Endemann <jahend44@gmail.com>
2025-02-26 18:42:16 -0600

    Update web.php
    Create Routes for the tasks using CRUD.

--- ToDo/todo/routes/web.php
+++ ToDo/todo/routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/tasks');
 });
+
+Route::get('/tasks', [App\Http\Controllers\TaskController::class, 'index'])->name('tasks.index');
+Route::get('/tasks/create', [App\Http\Controllers\TaskController::class, 'create'])-name('tasks.create');
+Route::post('/tasks', [App\Http\Controllers\TaskController::class, 'store'])-name('tasks.store');
+Route::get('/tasks/{task}', [App\Http\Controllers\TaskController::class, 'show'])-name('tasks.show');
+Route::get('/tasks/{task}/edit', [App\Http\Controllers\TaskController::class, 'edit'])-name('tasks.edit');
+Route::put('/tasks/{task}', [App\Http\Controllers\TaskController::class, 'update'])-name('tasks.update');
+Route::delete('/tasks/{task}', [App\Http\Controllers\TaskController::class, 'destroy'])-name('tasks.destroy');
+
