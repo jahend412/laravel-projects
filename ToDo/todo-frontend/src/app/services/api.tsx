acbd44df922f5919da1b1eebc9f7dfc63ce037e0
Josh Endemann <jahend44@gmail.com>
2025-03-03 20:02:41 -0600

    Fix: module not found fixed with @

--- ToDo/todo-frontend/src/app/services/api.tsx
+++ ToDo/todo-frontend/src/app/services/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Task, TaskFormData } from "../types";
+import { Task, TaskFormData } from "@/types";
 
 const api = axios.create({
   baseURL: "http://localhost:8000/api",
