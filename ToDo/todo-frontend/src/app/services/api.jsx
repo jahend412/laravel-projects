c4a42839b0c511f447eabe3c6cc21b42052f8a39
Josh Endemann <jahend44@gmail.com>
2025-03-03 19:27:32 -0600

    Create api.jsx
    Set up baseUrl and CRUD functionality for the todo list.

---
+++ ToDo/todo-frontend/src/app/services/api.jsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseUrl: "http://localhost:8000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const getTasks = () => api.get("/tasks");
+export const getTask = (id) => api.get(`/tasks/${id}`);
+export const createTask = (data) => api.post("/tasks", data);
+export const updateTask = (id, data) => api.put(`/tasks/${id}`, data);
+export const deleteTask = (id) => api.delete(`/tasks/${id}`);
+export const toggleComplete = (id, completed) =>
+  api.put(`/taks/${id}`, { completed: !completed });
+
+export default api;
